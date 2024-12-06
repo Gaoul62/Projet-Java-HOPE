d29f64f4d8cc674ddbd8e0cb45fb00f06b4eeb59
Rayan CAPALDI <gaoul6243@gmail.com>
2024-12-06 10:01:56 +0100

    init

---
+++ src/test/java/atln72/hope/HopeApplicationTests.java
@@ -0,0 +1,13 @@
+package atln72.hope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
