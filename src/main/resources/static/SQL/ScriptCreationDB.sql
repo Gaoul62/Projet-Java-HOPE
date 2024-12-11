d88c60606002fe8e7821b90af3f501188a3c294a
Rayan CAPALDI <gaoul6243@gmail.com>
2024-12-11 10:13:03 +0100

    Respect names normes

--- src/main/resources/static/SQL/ScriptCreationDB.sql
+++ src/main/resources/static/SQL/ScriptCreationDB.sql
@@ -4,9 +4,9 @@ CREATE DATABASE db_altn72_hope;
 
 USE db_altn72_hope;
 
-DROP TABLE IF EXISTS studenttools;
+DROP TABLE IF EXISTS StudentTool;
 
-CREATE TABLE studenttools (
+CREATE TABLE StudentTool (
     toolID INT PRIMARY KEY NOT NULL,
     title VARCHAR(50) NOT NULL,
     domainName VARCHAR(50) NOT NULL,
@@ -16,11 +16,11 @@ CREATE TABLE studenttools (
     accessInstruction VARCHAR(500) NOT NULL
 );
 
-DROP TABLE IF EXISTS userfeedback;
+DROP TABLE IF EXISTS UserFeedback;
 
-CREATE TABLE userfeedback (
+CREATE TABLE UserFeedback (
     feedbackID INT PRIMARY KEY NOT NULL,
     content VARCHAR(250) NOT NULL,
     toolID INT NOT NULL,
-    CONSTRAINT FK_userfeedback_studenttools FOREIGN KEY (toolID) REFERENCES studenttools(toolID) ON DELETE CASCADE
+    CONSTRAINT FK_UserFeedback_StudentTool FOREIGN KEY (toolID) REFERENCES StudentTool(toolID) ON DELETE CASCADE
 );
