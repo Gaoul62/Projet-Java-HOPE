a4fd08a4b4f17f06adcf89414b0ad8e0c8fb884f
Rayan Capaldi <gaoul6243@gmail.com>
2025-01-15 11:03:24 +0100

    Add User Context

---
+++ src/main/java/atln72/hope/util/UserContext.java
@@ -0,0 +1,8 @@
+package atln72.hope.util;
+
+import atln72.hope.model.UserAppEntity;
+
+public class UserContext {
+    // This is a singleton class that holds the current user
+    public static UserAppEntity currentUser;
+}
