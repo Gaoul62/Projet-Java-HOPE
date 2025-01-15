32e2568a42048753d98fd4424c39b8345fe402b4
Gaoul62 <gaoul6243@gmail.com>
2025-01-15 20:14:47 +0100

    Login Page

--- src/main/java/atln72/hope/util/UserContext.java
+++ src/main/java/atln72/hope/util/UserContext.java
@@ -5,4 +5,8 @@ import atln72.hope.model.UserAppEntity;
 public class UserContext {
     // This is a singleton class that holds the current user
     public static UserAppEntity currentUser;
+
+    public static void setCurrentUser(UserAppEntity user) {
+        currentUser = user;
+    }
 }
